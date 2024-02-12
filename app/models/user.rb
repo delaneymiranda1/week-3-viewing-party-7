b2435a65235c8656236ec30042b51238b8268414
Miranda Delaney <delaney.miranda1@gmail.com>
2024-02-12 13:10:05 -0700

    US 2 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User <ApplicationRecord 
-  validates_presence_of :email, :name, :password, :password_confirmation
+  validates_presence_of :email, uniqueness: true, presence: true
+  validates_presence_of :name, presence: true
+  validates_presence_of :password 
+  validates_presence_of :password_confirmation
+  validates_confirmation_of :password
   validates_uniqueness_of :email
   has_many :viewing_parties
 
