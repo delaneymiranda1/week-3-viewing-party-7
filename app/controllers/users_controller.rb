8dc71c62d0bacbbc5f3ccfd76a9cab8268ace9cf
Miranda Delaney <delaney.miranda1@gmail.com>
2024-02-12 13:02:14 -0700

    complete US 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController <ApplicationController 
   def new 
-    @user = User.new()
+    @user = User.new
   end 
 
   def show 
@@ -8,7 +8,7 @@ class UsersController <ApplicationController
   end 
 
   def create 
-    user = User.create(user_params)
+    user = User.new(user_params)
     if user.save
       redirect_to user_path(user)
     else  
@@ -20,6 +20,6 @@ class UsersController <ApplicationController
   private 
 
   def user_params 
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end 
 end 
--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
+  #   As a visitor 
+  # When I visit `/register`
+  # I see a form to fill in my name, email, password, and password confirmation.
+  # When I fill in that form with my name, email, and matching passwords,
+  # I'm taken to my dashboard page `/users/:id`
 
 RSpec.describe "User Registration" do
   it 'can create a user with a name and unique email' do
@@ -6,6 +11,8 @@ RSpec.describe "User Registration" do
 
     fill_in :user_name, with: 'User One'
     fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with: 'password123'
+    fill_in :user_password_confirmation, with: 'password123'
     click_button 'Create New User'
 
     expect(current_path).to eq(user_path(User.last.id))
@@ -13,15 +20,18 @@ RSpec.describe "User Registration" do
   end 
 
   it 'does not create a user if email isnt unique' do 
-    User.create(name: 'User One', email: 'notunique@example.com')
+    User.create(name: 'User One', email: 'notunique@example.com', password: 'password123', password_confirmation: 'password123')
 
     visit register_path
     
     fill_in :user_name, with: 'User Two'
     fill_in :user_email, with:'notunique@example.com'
+    fill_in :user_password, with: 'password123'
+    fill_in :user_password_confirmation, with: 'password123'
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
   end
+
 end
